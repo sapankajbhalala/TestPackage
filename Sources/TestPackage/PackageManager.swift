f31ffd2b9237227394a6e63416e9a161ec9801f8
Pankaj Bhalala <pankaj.bhalala.sa@gmail.com>
2020-05-29 18:45:06 +0530

    add palindromeSequence solution

--- Sources/TestPackage/PackageManager.swift
+++ Sources/TestPackage/PackageManager.swift
@@ -27,4 +27,28 @@ public struct PackageManager {
         }
         return arrEven
     }
+    
+    /*
+     /// ---palindromeSequence---
+     You are given an array numbers. Expand the numbers into an a palindrome landscape of mountains. For numbers = [2,3,2,4], palindromeMountains = [1,2,1,2,3,1,2,1,2,3,4,3,2,1]
+     **/
+    public static func palindromeSequence(numbers:[Int]) -> [Int] {
+        var mountains = [Int]()
+        for i in 0..<numbers.count {
+            var j = 1
+            if i != 0 {j = 2}
+            while j <= numbers[i] {
+                mountains.append(j)
+                j += 1
+            }
+            j -= 1
+            repeat {
+                j -= 1
+                mountains.append(j)
+            } while j > 1
+
+        }
+        return mountains
+    }
+
 };
